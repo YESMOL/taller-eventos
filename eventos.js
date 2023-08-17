1a700c1ea17ab0edbb10f7e2604df38efc351fea
YESMOL <81719487+YESMOL@users.noreply.github.com>
2023-08-16 21:42:51 -0300

    creacion de alerta

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var miDiv = document.getElementById('miDiv');
+    miDiv.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
